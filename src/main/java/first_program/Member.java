3096e34f2e68c41863dcd5bcd9fc559781017a45
ivanbyone <ivanbyone@gmail.com>
2024-09-12 15:24:38 +0300

    First JAVA program

---
+++ src/main/java/first_program/Member.java
@@ -0,0 +1,9 @@
+package first_program;
+
+public class Member {
+    public static void main(String[] args) {
+        // Output "Hello, World!"
+        // This's my first program in JAVA world ;)
+        System.out.println("Hello, World!");
+    }
+}
